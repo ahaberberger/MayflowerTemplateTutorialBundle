2456f8abd49ac0f8e8e784ab75741ca9d504908e
Andreas Haberberger <github@haberberger.com>
2014-02-13 13:49:54 +0000

    Basic Setup

--- DependencyInjection/MayflowerTemplateTutorialExtension.php
+++ DependencyInjection/MayflowerTemplateTutorialExtension.php
@@ -6,13 +6,15 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class MayflowerTemplateTutorialExtension extends Extension
+class MayflowerTemplateTutorialExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +26,11 @@ class MayflowerTemplateTutorialExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = Yaml::parse( __DIR__ . '/../Resources/config/ezpublish.yml' );
+        $container->prependExtensionConfig( 'ezpublish', $config );
     }
 }
